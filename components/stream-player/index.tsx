5becda4f204d39013aa369a6e41c2910b3f34895
SpEXterXD <nirmalyapal404@gmail.com>
2024-05-09 00:14:14 +0530

    Added Community And Minor Bug Fix And Changes

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -10,8 +10,8 @@ import { cn } from "@/lib/utils";
 
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 
-import { Video } from "./video";
-import { Chat } from "./chat";
+import { Video, VideoSkeleton } from "./video";
+import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 
 interface StreamPlayerProps {
@@ -37,9 +37,7 @@ export const StreamPlayer = ({
 
     if (!token || !name || !identity) {
         return (
-            <div>
-                Cannot Watch The Stream;
-            </div>
+            <StreamPlayerSkeleton />
         )
     }
 
@@ -81,6 +79,20 @@ export const StreamPlayer = ({
                     />
                 </div>
             </LiveKitRoom>
+        </div>
+    )
+}
+
+export const StreamPlayerSkeleton = () => {
+    return (
+        <div className="grid gird-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+            <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+                <VideoSkeleton />
+                {/* HeaderSkeleton */}
+            </div>
+            <div className="col-span-1 bg-background">
+                <ChatSkeleton />
+            </div>
         </div>
     )
 }
